8b997b3d2e621d9ed7600cbb6449fa810c012fc9
mack <13430264737@qq.com>
2024-02-22 16:23:46 +0800

    引入vant,并设置设配移动端

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
+    <vant-button type="primary">123</vant-button>
 </template>
 <script lang="ts" setup>
-
+import { Button as vantButton} from 'vant';
 </script>
 <style lang="less" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './style/main.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import 'vant/lib/index.css';
+import './style/main.scss'
 const app = createApp(App)
 
 app.use(createPinia())
